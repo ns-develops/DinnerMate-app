980537757ae77f8d7ba81769a8063457d9f712f2
ns-develops <test.tt9061030@gmail.com>
2025-03-19 22:02:30 +0100

    added two rectangleboxes, better view

--- DinnerMate-app/Views/RandomRecipeSelectionView.swift
+++ DinnerMate-app/Views/RandomRecipeSelectionView.swift
@@ -5,13 +5,12 @@
 //  Created by Natalie S on 2025-03-19.
 //
 
-
 import SwiftUI
 
 struct RandomRecipeSelectionView: View {
     
     @ObservedObject var viewModel: RecipeViewModel
-    @State private var randomRecipe: Recipe? 
+    @State private var randomRecipe: Recipe?
     @State private var categoryInput: String = ""
     
     var body: some View {
@@ -20,47 +19,65 @@ struct RandomRecipeSelectionView: View {
                 .font(.headline)
                 .padding()
             
-            // Knappar för att slumpa recept
-            Button(action: {
-                // Slumpa utan kategori
-                if let randomRecipe = viewModel.getRandomRecipe() {
-                    self.randomRecipe = randomRecipe
-                }
-            }) {
-                Text("Slumpa ett recept utan kategori")
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-            .padding()
-
-          
-            TextField("Ange kategori (t.ex. kött, vegetariskt)", text: $categoryInput)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
             
-            Button(action: {
-           
-                let category = categoryInput.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+            ZStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.blue.opacity(0.1))
+                    .frame(height: 100)
+                    .shadow(radius: 5)
                 
-                if !category.isEmpty {
-                    if let randomRecipe = viewModel.getRandomRecipe(fromCategory: category) {
+                Button(action: {
+                    // Slumpa utan kategori
+                    if let randomRecipe = viewModel.getRandomRecipe() {
                         self.randomRecipe = randomRecipe
-                    } else {
-                        self.randomRecipe = nil
+                    }
+                }) {
+                    Text("Slumpa ett recept utan kategori")
+                        .foregroundColor(.blue)
+                        .fontWeight(.bold)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 20)
+            
+        
+            ZStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.green.opacity(0.1))
+                    .frame(height: 100)
+                    .shadow(radius: 5)
+                
+                VStack {
+                    TextField("Ange kategori (t.ex. kött, vegetariskt)", text: $categoryInput)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                    
+                    Button(action: {
+                        let category = categoryInput.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+                        
+                        if !category.isEmpty {
+                            if let randomRecipe = viewModel.getRandomRecipe(fromCategory: category) {
+                                self.randomRecipe = randomRecipe
+                            } else {
+                                self.randomRecipe = nil
+                            }
+                        }
+                    }) {
+                        Text("Slumpa recept från kategori")
+                            .foregroundColor(.green)
+                            .fontWeight(.bold)
+                            .padding()
+                            .frame(maxWidth: .infinity)
                     }
                 }
-            }) {
-                Text("Slumpa recept från kategori")
-                    .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
             }
-            .padding()
-
+            .padding(.horizontal)
+            .padding(.top, 10)
             
+          
             if let randomRecipe = randomRecipe {
                 NavigationLink(destination: RandomRecipeView(recipe: randomRecipe)) {
                     Text("Visa slumpat recept")
@@ -69,7 +86,7 @@ struct RandomRecipeSelectionView: View {
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
-                .padding()
+                .padding(.top, 20)
             }
 
             Spacer()
